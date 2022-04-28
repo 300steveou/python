a4d5127213aeed7447ea235c9a64b1801ba9ccf4
Steve Ou <300steveou@gmail.com>
2022-04-28 17:38:30 +0800

    commit

--- inhertiance.py
+++ inhertiance.py
@@ -1,6 +1,11 @@
+from Animal import Animal
 from classObject import People
 
 people1= People("Clock",True,"1430")
 # people1.yellString("叫什麼叫")
 
 people1.yell("yell what?")
+
+# People是否為Animal的子類別
+print(issubclass(People,Animal))
+
